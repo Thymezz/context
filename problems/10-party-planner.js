2aa733ae86ada12e82e70cd1eb02073ffaf79e39
Thomas Vogel <Lenex5109@yahoo.com>
2023-11-07 22:40:11 -0600

    10

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,21 @@
-// Your code here 
+class PartyPlanner {
+	constructor() {
+	  this.guestList = [];
+	}
 
+	addToGuestList(name) {
+	  this.guestList.push(name);
+	}
+
+	throwParty() {
+	  if (this.guestList.length === 0) {
+		return "Gotta add people to the guest list";
+	  } else {
+		const guestNames = this.guestList.join(" and ");
+		return `Welcome to the party ${guestNames}`;
+	  }
+	}
+  }
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
